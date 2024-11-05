0d187accfd03db7ed1647ef081f24fef9c4c4af2
nmiton <nathan.miton@orange.fr>
2024-11-05 09:19:58 +0100

    upd

--- dist/index.js
+++ dist/index.js
@@ -1,13 +1,17 @@
 "use strict";
 
 Object.defineProperty(exports, "__esModule", {
-  value: true
+	value: true,
 });
 Object.defineProperty(exports, "Modal", {
-  enumerable: true,
-  get: function get() {
-    return _modal["default"];
-  }
+	enumerable: true,
+	get: function get() {
+		return _modal["default"];
+	},
 });
 var _modal = _interopRequireDefault(require("./modal"));
-function _interopRequireDefault(e) { return e && e.__esModule ? e : { "default": e }; }
+function _interopRequireDefault(e) {
+	return e && e.__esModule ? e : { default: e };
+}
+
+exports.Modal = _modal.default;
